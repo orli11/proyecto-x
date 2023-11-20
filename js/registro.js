88302e7d37db130d74fa9628a2b53ecb7bb35226
AlejandroRG12 <garcia4312@hotmail.com>
2023-11-20 11:24:58 -0600

    Update CSS and JavaScript files

--- js/registro.js
+++ js/registro.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () =>{
     window.onload = function() {
-        var diaSelect = document.getElementById('dia');
-        var mesSelect = document.getElementById('mes');
-        var anoSelect = document.getElementById('ano');
+        let diaSelect = document.getElementById('dia');
+        let mesSelect = document.getElementById('mes');
+        let anoSelect = document.getElementById('ano');
 
         for (var i = 1; i <= 31; i++) {
             diaSelect.innerHTML += `<option value="${i}">${i}</option>`;
         }
 
-        var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
         for (var i = 0; i < meses.length; i++) {
             mesSelect.innerHTML += `<option value="${i + 1}">${meses[i]}</option>`;
         }
